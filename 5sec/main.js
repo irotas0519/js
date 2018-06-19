2c0ecbd26f592e7ae6eb765c207e30d7c36ab222
irotas <chen5tas19@gmail.com>
2018-06-19 17:57:40 +0900

    5秒当てゲームです

---
+++ 5sec/main.js
@@ -0,0 +1,48 @@
+(function() {
+    'user strict';
+
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+    var result = document.getElementById('result');
+    var startTime;
+    var isStarted = false;
+
+
+
+    start.addEventListener('click', function() {
+        if (isStarted === true) {
+            return;
+        }
+        isStarted = true;
+        startTime = Date.now();
+        this.className = 'pushed';
+        stop.className = '';
+        result.textContent = '0.000';
+        result.className = 'standby';
+    });
+
+    stop.addEventListener('click', function() {
+        var elapsedTime;
+        var diff;
+        if (isStarted === false) {
+            return;
+        }
+        isStarted = false;
+        this.className = 'pushed';
+        start.className = '';
+        result.className = '';
+
+
+        elapsedTime = (Date.now() - startTime) / 1000;
+        result.textContent = elapsedTime.toFixed(3);
+        diff= elapsedTime - 5.0;
+        if (diff > -1.0 && diff < 1.0) {
+            result.className = 'perfect';
+        }
+
+
+
+
+
+    });
+})();
